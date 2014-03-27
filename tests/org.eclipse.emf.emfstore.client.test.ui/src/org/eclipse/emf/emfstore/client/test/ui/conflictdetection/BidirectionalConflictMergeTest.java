6b9dd68ea1ec943610acdf20cdf46297ee33457a
Edgar Mueller <edgar.m.mueller@gmail.com>
2014-03-27 11:58:51 +0100

    Bug 421361: Conflicting bidirectional references can cause ClassCastException

--- tests/org.eclipse.emf.emfstore.client.test.ui/src/org/eclipse/emf/emfstore/client/test/ui/conflictdetection/BidirectionalConflictMergeTest.java
+++ tests/org.eclipse.emf.emfstore.client.test.ui/src/org/eclipse/emf/emfstore/client/test/ui/conflictdetection/BidirectionalConflictMergeTest.java
@@ -11,6 +11,9 @@
  ******************************************************************************/
 package org.eclipse.emf.emfstore.client.test.ui.conflictdetection;
 
+import static org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory.withText;
+import static org.eclipse.swtbot.swt.finder.waits.Conditions.waitForShell;
+
 import java.util.concurrent.Callable;
 
 import org.eclipse.emf.emfstore.client.ESLocalProject;
@@ -21,11 +24,17 @@ import org.eclipse.emf.emfstore.client.test.ui.controllers.AbstractUIControllerT
 import org.eclipse.emf.emfstore.client.util.RunESCommand;
 import org.eclipse.emf.emfstore.common.model.ESModelElementId;
 import org.eclipse.emf.emfstore.internal.client.model.impl.AutoOperationWrapper;
+import org.eclipse.emf.emfstore.internal.client.ui.controller.UIUpdateProjectController;
 import org.eclipse.emf.emfstore.internal.common.ExtensionRegistry;
 import org.eclipse.emf.emfstore.server.exceptions.ESException;
 import org.eclipse.emf.emfstore.test.model.TestElement;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-import org.junit.Ignore;
+import org.eclipse.swtbot.swt.finder.results.VoidResult;
+import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.hamcrest.Matcher;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -41,7 +50,7 @@ import org.junit.runner.RunWith;
 public class BidirectionalConflictMergeTest extends AbstractUIControllerTestWithCommit {
 
 	@Override
-	@Ignore
+	// @Ignore
 	@Test
 	public void testController() throws ESException {
 
@@ -73,8 +82,33 @@ public class BidirectionalConflictMergeTest extends AbstractUIControllerTestWith
 			localProject.getModelElementId(child),
 			localProject.getModelElementId(testElement3));
 
-		updateCopy();
-		commit();
+		UIThreadRunnable.asyncExec(new VoidResult() {
+			public void run() {
+				final UIUpdateProjectController updateProjectController = new UIUpdateProjectController(
+					bot.getDisplay().getActiveShell(),
+					getCopy());
+				updateProjectController.execute();
+			}
+		});
+		final Matcher<Shell> matcher = withText("Update");
+		bot.waitUntil(waitForShell(matcher));
+		bot.button("OK").click();
+
+		bot.waitUntil(new DefaultCondition() {
+
+			public boolean test() throws Exception {
+				return bot.shell("Merge Wizard") != null;
+			}
+
+			public String getFailureMessage() {
+				return "Merge wizard did not appear";
+			}
+		});
+
+		final SWTBotShell shell2 = bot.shell("Merge Wizard");
+		shell2.bot().button(1).click();
+		shell2.bot().button(4).click();
+
 	}
 
 	protected void changeContainer(final ESLocalProject localProject, final ESModelElementId childId,
