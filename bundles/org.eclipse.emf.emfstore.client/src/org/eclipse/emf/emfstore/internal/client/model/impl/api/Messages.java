3af9defe50b2fb7baf7a7863da40d5f86bed6236
Edgar Mueller <edgar.m.mueller@gmail.com>
2014-08-01 14:28:11 +0200

    Bug 424018 - Externalize strings

--- bundles/org.eclipse.emf.emfstore.client/src/org/eclipse/emf/emfstore/internal/client/model/impl/api/Messages.java
+++ bundles/org.eclipse.emf.emfstore.client/src/org/eclipse/emf/emfstore/internal/client/model/impl/api/Messages.java
@@ -17,7 +17,7 @@ import org.eclipse.osgi.util.NLS;
  * API implementation related messages.
  * 
  * @author emueller
- * 
+ * @generated
  */
 public final class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.emf.emfstore.internal.client.model.impl.api.messages"; //$NON-NLS-1$
